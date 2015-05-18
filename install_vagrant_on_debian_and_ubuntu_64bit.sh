d234f82bdb54d46517f8295c6ac49decf2950c4b
w3aran <developer@FlightNetworks-MacBook-Pro-2.local>
2015-05-18 09:13:41 -0400

    Initial commit

---
+++ install_vagrant_on_debian_and_ubuntu_64bit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+PACKAGE_FILE_NAME=vagrant_1.7.2_x86_64.deb
+PACKAGE_DOWNLOAD_LINK=https://dl.bintray.com/mitchellh/vagrant/$PACKAGE_FILE_NAME
+
+wget $PACKAGE_DOWNLOAD_LINK
+sudo dpkg -i $PACKAGE_FILE_NAME
+rm $PACKAGE_FILE_NAME
