b8c8a153881dfa500494de90014f87e4f4bce2af
w3aran <w3aran@gmail.com>
2015-10-26 13:49:13 -0400

    Version upgraded from 1.7.2 to 1.7.4

--- install_vagrant_on_debian_and_ubuntu_64bit.sh
+++ install_vagrant_on_debian_and_ubuntu_64bit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGE_FILE_NAME=vagrant_1.7.2_x86_64.deb
+PACKAGE_FILE_NAME=vagrant_1.7.4_x86_64.deb
 PACKAGE_DOWNLOAD_LINK=https://dl.bintray.com/mitchellh/vagrant/$PACKAGE_FILE_NAME
 
 wget $PACKAGE_DOWNLOAD_LINK
